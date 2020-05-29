e6ab0e003adb0dfec804285d1f75f0d7bee04cbb
nmay <nmay>
2020-05-29 17:10:30 +0000

    Getting started with Rubyfossil add yaml.dump

--- data.rb
+++ data.rb
@@ -1,10 +1,36 @@
 # Created: 2020-05-28
-# Revised: 2020-05-28
+# Revised: 2020-05-29
 
-class project
-end
-class task
+require 'yaml'
+
+## TODO $projects_and_tasks
+
+# store in arrays by keyword so maybe hash tables?
+# potiental issue of syncing keyword in object w/ stored keyword in hash? Or simply don't duplicate?
+
+class Project
+  def initialize(title, keyword, life_context)
+    @title = title
+    @keyword = keyword
+    @life_context = life_context
+  end
 end
 
-class generic-project-task
+test = Project.new("test project","tp","personal")
+puts test
+
+class Task
 end
+
+#class generic-project-task
+#end
+
+m = YAML.load(File.read('yaml.dump'))
+# m = File.open('/marshal.dump', 'wb') { |f| f.write(Marshal.dump(m)) }
+
+m = [1, 2, 3, 4, "Ruby"]
+
+#sleep 0.9
+
+#File.open('yaml.dump', 'w') { |f| f.write(YAML.dump(m)) }
+#File.open('marshal.dump', 'wb') { |f| f.write(Marshal.dump(m)) }
