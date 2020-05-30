d4cdbe38090064b8475c38e54cbc870414315621
nmay <nmay>
2020-05-30 23:50:48 +0000

    lisp->ruby

---
+++ commands.rb
@@ -0,0 +1,18 @@
+# Created: 2020-05-30
+# Revised: 2020-05-30
+
+# Every piece of input into extbrain should start with a command or be a command.
+# The exception is, if no command, add new task is assumed.
+# For example: lp work
+# For example: exit
+# Add new task: some task
+(define (dispatch-command user-input)
+  (case user-input
+    ("exit" (setq exit-extbrain true))
+    (true (create-task user-input))))
+
+
+(define (command-loop)
+  (until exit-extbrain
+    (print "> ")
+    (dispatch-command (read-line))))
