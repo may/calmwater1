c904ce21441a777d6754bdaa0349e8b1e0023774
nmay <nmay>
2020-08-21 01:55:24 +0000

    File storage location configurable now. I moved it to /net/u/11/n/nmay so sshfs + backup software will pickup daily changes.

--- config.rb
+++ config.rb
@@ -1,20 +1,20 @@
 # Created: 2020-06-06
 # Revised: 2020-08-20
 $log_command_usage_locally = false
-$save_file_in_subdirectory = "" # set to "/extbrain" if you want it to live in there
-$data_file_command_usage = "#{Dir.home}/extbrain/extbrain_command_usage.yaml"
-$save_file_last_weekly_review_done = "#{Dir.home}/extbrain/extbrain_last_weekly_review.yaml"
+$save_file_directory = "" # set to "/extbrain" if you want it to live in there
+$data_file_command_usage = "#{Dir.home}#{$save_file_directory}/extbrain_command_usage.yaml"
+$save_file_last_weekly_review_done = "#{Dir.home}#{$save_file_directory}/extbrain_last_weekly_review.yaml"
 
 # Disable this if you don't work 8-5, or don't find automatic contexts useful. It's also adjustable, below.
 $time_sensitive_life_context = true
 # unix/linux/osx only w/ home
-$save_file_habits = "#{Dir.home}/extbrain/extbrain_habits.yaml"
-$save_file_projects = "#{Dir.home}/extbrain/extbrain_projects.yaml"
-$save_file_tasks = "#{Dir.home}/extbrain/extbrain_tasks.yaml"
-$lockfile = "#{Dir.home}/extbrain/lockfile-extbrain.txt" # unix/linux/osx only?
+$save_file_habits = "#{Dir.home}#{$save_file_directory}/extbrain_habits.yaml"
+$save_file_projects = "#{Dir.home}#{$save_file_directory}/extbrain_projects.yaml"
+$save_file_tasks = "#{Dir.home}#{$save_file_directory}/extbrain_tasks.yaml"
+$lockfile = "#{Dir.home}#{$save_file_directory}/lockfile-extbrain.txt" # unix/linux/osx only?
 
-$archive_file_projects = "#{Dir.home}/extbrain/extbrain_#{Time.now.year}_projects_completed_or_deleted.yaml"
-$archive_file_tasks = "#{Dir.home}/extbrain/extbrain_#{Time.now.year}_non-project_tasks_completed_or_deleted.yaml"
+$archive_file_projects = "#{Dir.home}#{$save_file_directory}/extbrain_#{Time.now.year}_projects_completed_or_deleted.yaml"
+$archive_file_tasks = "#{Dir.home}#{$save_file_directory}/extbrain_#{Time.now.year}_non-project_tasks_completed_or_deleted.yaml"
 
 
 $color_only = true # If perfer b&w, if colorblind, or if using Windows, set this to false.
